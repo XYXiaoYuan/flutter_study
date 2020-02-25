0e9b2bc0e567f0dc8a46c5c97be7a7dece7e9e99
Bruceyuan <664995775@qq.com>
2020-02-25 17:53:16 +0800

    AspectRatio：宽高比

--- flutter_study/lib/demo/layout_demo.dart
+++ flutter_study/lib/demo/layout_demo.dart
@@ -7,63 +7,65 @@ class LayoutDemo extends StatelessWidget {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
-          Stack(
-            alignment: Alignment.topLeft,
-            children: <Widget>[
-              SizedBox(
-                  width: 200.0,
-                  height: 300.0,
-                  child: Container(
-                      alignment: Alignment.topLeft,
-                      decoration: BoxDecoration(
-                        color: Color.fromRGBO(3, 54, 255, 1.0),
-                        borderRadius: BorderRadius.circular(8.0)
-                      ),
-                      child: Icon(Icons.ac_unit, color: Colors.white, size: 32.0)
-                  )
-              ),
-              SizedBox(height: 32.0),
-              SizedBox(
-                  width: 100.0,
-                  height: 100.0,
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: Color.fromRGBO(3, 54, 255, 1.0),
-                      shape: BoxShape.circle,
-                      gradient: RadialGradient(
-                        colors: [
-                          Color.fromRGBO(7, 102, 255, 1.0),
-                          Color.fromRGBO(3, 54, 255, 1.0)
-                        ]
-                      ),
-                    ),
-                  child: Icon(Icons.brightness_2, color: Colors.white, size: 32.0)
-                )
-              ),
-              Positioned(
-                right: 20.0,
-                top: 120.0,
-                child: Icon(Icons.ac_unit, color: Colors.white, size: 32.0)
-              ),
-              Positioned(
-                right: 70.0,
-                top: 180.0,
-                child: Icon(Icons.ac_unit, color: Colors.white, size: 32.0)
-              ),
-              Positioned(
-                right: 30.0,
-                top: 230.0,
-                child: Icon(Icons.ac_unit, color: Colors.white, size: 32.0)
-              ),
-              Positioned(
-                right: 90.0,
-                bottom: 20.0,
-                child: Icon(Icons.ac_unit, color: Colors.white, size: 32.0)
-              )
-            ],
+          AspectRatio(
+            aspectRatio: 16.0/9.0,
+            child: Container(
+              color: Color.fromRGBO(3, 54, 255, 1.0),
+            ),
           )
         ],
       ),
+    );
+  }
+}
+
+class StackDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      alignment: Alignment.topLeft,
+      children: <Widget>[
+        SizedBox(
+            width: 200.0,
+            height: 300.0,
+            child: Container(
+                alignment: Alignment.topLeft,
+                decoration: BoxDecoration(
+                    color: Color.fromRGBO(3, 54, 255, 1.0),
+                    borderRadius: BorderRadius.circular(8.0)),
+                child: Icon(Icons.ac_unit, color: Colors.white, size: 32.0))),
+        SizedBox(height: 32.0),
+        SizedBox(
+            width: 100.0,
+            height: 100.0,
+            child: Container(
+                decoration: BoxDecoration(
+                  color: Color.fromRGBO(3, 54, 255, 1.0),
+                  shape: BoxShape.circle,
+                  gradient: RadialGradient(colors: [
+                    Color.fromRGBO(7, 102, 255, 1.0),
+                    Color.fromRGBO(3, 54, 255, 1.0)
+                  ]),
+                ),
+                child:
+                    Icon(Icons.brightness_2, color: Colors.white, size: 32.0))),
+        Positioned(
+            right: 20.0,
+            top: 120.0,
+            child: Icon(Icons.ac_unit, color: Colors.white, size: 32.0)),
+        Positioned(
+            right: 70.0,
+            top: 180.0,
+            child: Icon(Icons.ac_unit, color: Colors.white, size: 32.0)),
+        Positioned(
+            right: 30.0,
+            top: 230.0,
+            child: Icon(Icons.ac_unit, color: Colors.white, size: 32.0)),
+        Positioned(
+            right: 90.0,
+            bottom: 20.0,
+            child: Icon(Icons.ac_unit, color: Colors.white, size: 32.0)),
+      ],
     );
   }
 }
