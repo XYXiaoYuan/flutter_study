9a666b2bd9cacf025a2998e4975572e6278f4129
Bruceyuan <664995775@qq.com>
2020-02-25 23:51:58 +0800

    SliverList：列表视图

--- flutter_study/lib/demo/sliver_demo.dart
+++ flutter_study/lib/demo/sliver_demo.dart
@@ -10,7 +10,7 @@ class SliverDemo extends StatelessWidget {
           SliverSafeArea(
             sliver: SliverPadding(
               padding: EdgeInsets.all(8.0),
-              sliver: SliverGridDemo(),
+              sliver: SliverListDemo(),
             )
           ),
         ]
@@ -18,6 +18,62 @@ class SliverDemo extends StatelessWidget {
     );
   }
 }
+
+class SliverListDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SliverList(
+      delegate: SliverChildBuilderDelegate(
+        (BuildContext context, int index) {
+          return Padding(
+            padding: EdgeInsets.only(bottom: 32.0),
+            child: Material(
+              borderRadius: BorderRadius.circular(12.0),
+              elevation: 14.0,
+              shadowColor: Colors.grey.withOpacity(0.5),
+              child: Stack(
+                children: <Widget>[
+                  AspectRatio(
+                    aspectRatio: 16/9,
+                    child: Image.network(
+                      posts[index].imageUrl,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  Positioned(
+                    top: 32.0,
+                    left: 32.0,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          posts[index].title,
+                          style: TextStyle(
+                            fontSize: 20.0,
+                            color: Colors.white,
+                          )
+                        ),
+                        Text(
+                          posts[index].author,
+                          style: TextStyle(
+                            fontSize: 13.0,
+                            color: Colors.white,
+                          )
+                        ),
+                      ]
+                    )
+                  )
+                ],
+              )
+          )
+        );
+      },
+      childCount: posts.length,
+      ),
+    );
+  }
+}
+
 
 class SliverGridDemo extends StatelessWidget {
   @override
