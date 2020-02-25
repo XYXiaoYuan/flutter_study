7488e6d91c6d4c345073daa1022cab42f73d9e1f
Bruceyuan <664995775@qq.com>
2020-02-25 23:39:52 +0800

    SliverPadding（内边距） 与 SliverSafeArea（安全区）

--- flutter_study/lib/demo/sliver_demo.dart
+++ flutter_study/lib/demo/sliver_demo.dart
@@ -7,26 +7,37 @@ class SliverDemo extends StatelessWidget {
     return Scaffold(
       body: CustomScrollView(
         slivers: <Widget>[
-          SliverGrid(
-            delegate: SliverChildBuilderDelegate(
-              (BuildContext context, int index) {
-                return Container(
-                  child: Image.network(
-                    posts[index].imageUrl,
-                    fit: BoxFit.cover,
-                  )
-                );
-              },
-              childCount: posts.length,
-            ), 
-            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2,
-              crossAxisSpacing: 8.0,
-              mainAxisSpacing: 8.0,
-              childAspectRatio: 1.0,
-            ),
+          SliverSafeArea(
+            sliver: SliverPadding(
+              padding: EdgeInsets.all(8.0),
+              sliver: SliverGridDemo(),
+            )
           ),
         ]
+      ),
+    );
+  }
+}
+
+class SliverGridDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SliverGrid(
+      delegate: SliverChildBuilderDelegate(
+        (BuildContext context, int index) {
+          return Container(
+              child: Image.network(
+            posts[index].imageUrl,
+            fit: BoxFit.cover,
+          ));
+        },
+        childCount: posts.length,
+      ),
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        crossAxisSpacing: 8.0,
+        mainAxisSpacing: 8.0,
+        childAspectRatio: 1.0,
       ),
     );
   }
