0f1919ee39428985bd5acce408d8cc90639b9ce7
Bruceyuan <664995775@qq.com>
2020-02-25 11:12:54 +0800

    列表项目

--- flutter_study/lib/main.dart
+++ flutter_study/lib/main.dart
@@ -1,8 +1,58 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_study/model/post.dart';
 
 void main() => runApp(App());
 
 class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Home(),
+        theme: ThemeData(
+          primaryColor: Colors.yellow,
+        ));
+  }
+}
+
+class Home extends StatelessWidget {
+  Widget _listItemBuilder(BuildContext context, int index) {
+    return Container(
+      color: Colors.white,
+      margin: EdgeInsets.all(8.0),
+      child: Column(
+        children: <Widget>[
+          Image.network(posts[index].imageUrl),
+          SizedBox(height: 16.0),
+          Text(
+            posts[index].title,
+            style: Theme.of(context).textTheme.title,
+          ),
+          Text(
+            posts[index].author,
+            style: Theme.of(context).textTheme.subhead,
+          ),
+          SizedBox(height: 16.0),
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.grey[100],
+        appBar: AppBar(
+          title: Text('NINGHAO'),
+          elevation: 0.0,
+        ),
+        body: ListView.builder(
+          itemBuilder: _listItemBuilder,
+          itemCount: posts.length,
+        ));
+  }
+}
+
+class Hello extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -12,7 +62,7 @@ class App extends StatelessWidget {
         style: TextStyle(
           fontSize: 40.0,
           fontWeight: FontWeight.bold,
-          color: Colors.green,
+          color: Colors.black87,
         ),
       ),
     );
