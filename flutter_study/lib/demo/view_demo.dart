87a651e52c8bdddb904f7ddae0c59421751f1f00
Bruceyuan <664995775@qq.com>
2020-02-25 22:50:39 +0800

    PageView.builder 按需生成页面

--- flutter_study/lib/demo/view_demo.dart
+++ flutter_study/lib/demo/view_demo.dart
@@ -1,6 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_study/model/post.dart';
 
 class ViewDemo extends StatelessWidget {
+  Widget _pageItemBuilder(BuildContext context, int index) {
+    return Stack(
+      children: <Widget>[
+        SizedBox.expand(
+          child: Image.network(
+            posts[index].imageUrl,
+            fit: BoxFit.cover,
+          ),
+        ),
+        Positioned(
+          bottom: 8.0,
+          left: 8.0,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                posts[index].title,
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              Text(
+                posts[index].author,
+              ),
+            ]
+          ),
+        ),
+      ],
+    ); 
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return PageView.builder(
+      itemCount: posts.length,
+      itemBuilder: _pageItemBuilder,
+    );
+  }
+}
+
+class Cro {
+}
+
+class PageViewDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return PageView(
