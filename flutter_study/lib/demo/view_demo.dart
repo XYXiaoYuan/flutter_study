69bfadc9f340c9a55c0f3ccbf53bde8903951a80
Bruceyuan <664995775@qq.com>
2020-02-25 22:21:49 +0800

    1.PageView：页面视图 2.PageView：页面视图的属性

---
+++ flutter_study/lib/demo/view_demo.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class ViewDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return PageView(
+      // pageSnapping: true,
+      // reverse: false,
+      scrollDirection: Axis.horizontal,
+      onPageChanged: (currentPage) => debugPrint('Page: $currentPage'),
+      children: <Widget>[
+        Container(
+          color: Colors.brown[900],
+          alignment: Alignment(0.0, 0.0),
+          child: Text(
+            'ONE',
+            style: TextStyle(fontSize: 32.0, color: Colors.white),
+          ),
+        ),
+        Container(
+          color: Colors.grey[900],
+          alignment: Alignment(0.0, 0.0),
+          child: Text(
+            'TWO',
+            style: TextStyle(fontSize: 32.0, color: Colors.white),
+          ),
+        ),
+        Container(
+          color: Colors.blueGrey[900],
+          alignment: Alignment(0.0, 0.0),
+          child: Text(
+            'THREE',
+            style: TextStyle(fontSize: 32.0, color: Colors.white),
+          ),
+        ),
+      ],
+    );
+  }
+}
--- flutter_study/lib/main.dart
+++ flutter_study/lib/main.dart
@@ -4,6 +4,7 @@ import 'demo/layout_demo.dart';
 import 'demo/listview_demo.dart';
 import 'demo/bottom_navigation_bar_demo.dart';
 import 'demo/drawer_demo.dart';
+import 'demo/view_demo.dart';
 
 void main() => runApp(App());
 
@@ -24,7 +25,7 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
-        length: 3,
+        length: 4,
         child: Scaffold(
           backgroundColor: Colors.grey[100],
           appBar: AppBar(
@@ -42,6 +43,7 @@ class Home extends StatelessWidget {
                 Tab(icon: Icon(Icons.local_florist)),
                 Tab(icon: Icon(Icons.change_history)),
                 Tab(icon: Icon(Icons.directions_bike)),
+                Tab(icon: Icon(Icons.view_quilt)),
               ],
               indicatorColor: Colors.black54,
               indicatorSize: TabBarIndicatorSize.label,
@@ -53,6 +55,7 @@ class Home extends StatelessWidget {
               ListViewDemo(),
               BasicDemo(),
               LayoutDemo(),
+              ViewDemo()
           ]),
           drawer: DrawerDemo(),
           bottomNavigationBar: BottomNavigationBarDemo(),
