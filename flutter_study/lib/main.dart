4b074c0855d204b68f98a36620a6963f883119b0
Bruceyuan <664995775@qq.com>
2020-02-25 14:34:44 +0800

    用户帐户头部的背景图像

--- flutter_study/lib/main.dart
+++ flutter_study/lib/main.dart
@@ -64,6 +64,17 @@ class Home extends StatelessWidget {
                   currentAccountPicture: CircleAvatar(
                     backgroundImage: NetworkImage('https://resources.ninghao.org/images/wanghao.jpg')
                   ),
+                  decoration: BoxDecoration(
+                    color: Colors.yellow[400],
+                    image: DecorationImage(
+                      image: NetworkImage('https://resources.ninghao.org/images/childhood-in-a-picture.jpg'),
+                      fit: BoxFit.cover,
+                      colorFilter: ColorFilter.mode(
+                        Colors.yellow[400].withOpacity(0.6), 
+                        BlendMode.srcOver
+                      )
+                    )
+                  ),
                 ),
                 ListTile(
                   title: Text('Message', textAlign: TextAlign.right),
