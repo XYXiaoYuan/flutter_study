415983b03b0972f8970391849cbef78f38c36ad3
Bruceyuan <664995775@qq.com>
2020-02-25 13:15:30 +0800

    Drawer：抽屉（边栏）

--- flutter_study/lib/main.dart
+++ flutter_study/lib/main.dart
@@ -21,36 +21,44 @@ class Home extends StatelessWidget {
     return DefaultTabController(
         length: 3,
         child: Scaffold(
-            backgroundColor: Colors.grey[100],
-            appBar: AppBar(
-              leading: IconButton(
-                  icon: Icon(Icons.menu),
-                  tooltip: 'Navigation',
-                  onPressed: () => debugPrint('Navigation button is pressed.')),
-              title: Text('NINGHAO'),
-              actions: <Widget>[
-                IconButton(
-                    icon: Icon(Icons.search),
-                    tooltip: 'Search',
-                    onPressed: () => debugPrint('Search button is pressed.'))
+          backgroundColor: Colors.grey[100],
+          appBar: AppBar(
+            leading: IconButton(
+                icon: Icon(Icons.menu),
+                tooltip: 'Navigation',
+                onPressed: () => debugPrint('Navigation button is pressed.')),
+            title: Text('NINGHAO'),
+            actions: <Widget>[
+              IconButton(
+                  icon: Icon(Icons.search),
+                  tooltip: 'Search',
+                  onPressed: () => debugPrint('Search button is pressed.'))
+            ],
+            elevation: 0.0,
+            bottom: TabBar(
+              unselectedLabelColor: Colors.black38,
+              tabs: <Widget>[
+                Tab(icon: Icon(Icons.local_florist)),
+                Tab(icon: Icon(Icons.change_history)),
+                Tab(icon: Icon(Icons.directions_bike)),
               ],
-              elevation: 0.0,
-              bottom: TabBar(
-                unselectedLabelColor: Colors.black38,
-                tabs: <Widget>[
-                  Tab(icon: Icon(Icons.local_florist)),
-                  Tab(icon: Icon(Icons.change_history)),
-                  Tab(icon: Icon(Icons.directions_bike)),
-                ],
-                indicatorColor: Colors.black54,
-                indicatorSize: TabBarIndicatorSize.label,
-                indicatorWeight: 1.0,
-              ),
+              indicatorColor: Colors.black54,
+              indicatorSize: TabBarIndicatorSize.label,
+              indicatorWeight: 1.0,
             ),
-            body: TabBarView(children: <Widget>[
-              Icon(Icons.local_florist, size: 128.0, color: Colors.black12),
-              Icon(Icons.change_history, size: 128.0, color: Colors.black12),
-              Icon(Icons.directions_bike, size: 128.0, color: Colors.black12),
-            ])));
+          ),
+          body: TabBarView(children: <Widget>[
+            Icon(Icons.local_florist, size: 128.0, color: Colors.black12),
+            Icon(Icons.change_history, size: 128.0, color: Colors.black12),
+            Icon(Icons.directions_bike, size: 128.0, color: Colors.black12),
+          ]),
+          drawer: Container(
+              color: Colors.white,
+              padding: EdgeInsets.all(8.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[Text('This is drawer')],
+              )),
+        ));
   }
 }
