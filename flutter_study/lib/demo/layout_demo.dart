793e779e08f121e743df42303f52f49ccc26c5fc
Bruceyuan <664995775@qq.com>
2020-02-25 17:46:06 +0800

    Stack：一摞小部件

--- flutter_study/lib/demo/layout_demo.dart
+++ flutter_study/lib/demo/layout_demo.dart
@@ -7,29 +7,60 @@ class LayoutDemo extends StatelessWidget {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
-          SizedBox(
-            width: 200.0,
-            height: 300.0,
-            child: Container(
-              alignment: Alignment.topLeft,
-              decoration: BoxDecoration(
-                color: Color.fromRGBO(3, 54, 255, 1.0),
-                borderRadius: BorderRadius.circular(8.0)
+          Stack(
+            alignment: Alignment.topLeft,
+            children: <Widget>[
+              SizedBox(
+                  width: 200.0,
+                  height: 300.0,
+                  child: Container(
+                      alignment: Alignment.topLeft,
+                      decoration: BoxDecoration(
+                        color: Color.fromRGBO(3, 54, 255, 1.0),
+                        borderRadius: BorderRadius.circular(8.0)
+                      ),
+                      child: Icon(Icons.ac_unit, color: Colors.white, size: 32.0)
+                  )
               ),
-              child: Icon(Icons.ac_unit, color: Colors.white, size: 32.0)
-            )
-          ),
-          SizedBox(height: 32.0),
-          SizedBox(
-            width: 100.0,
-            height: 100.0,
-            child: Container(
-              decoration: BoxDecoration(
-                color: Color.fromRGBO(3, 54, 255, 1.0),
-                borderRadius: BorderRadius.circular(8.0)
+              SizedBox(height: 32.0),
+              SizedBox(
+                  width: 100.0,
+                  height: 100.0,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Color.fromRGBO(3, 54, 255, 1.0),
+                      shape: BoxShape.circle,
+                      gradient: RadialGradient(
+                        colors: [
+                          Color.fromRGBO(7, 102, 255, 1.0),
+                          Color.fromRGBO(3, 54, 255, 1.0)
+                        ]
+                      ),
+                    ),
+                  child: Icon(Icons.brightness_2, color: Colors.white, size: 32.0)
+                )
               ),
-              child: Icon(Icons.brightness_2, color: Colors.white, size: 32.0)
-            )
+              Positioned(
+                right: 20.0,
+                top: 120.0,
+                child: Icon(Icons.ac_unit, color: Colors.white, size: 32.0)
+              ),
+              Positioned(
+                right: 70.0,
+                top: 180.0,
+                child: Icon(Icons.ac_unit, color: Colors.white, size: 32.0)
+              ),
+              Positioned(
+                right: 30.0,
+                top: 230.0,
+                child: Icon(Icons.ac_unit, color: Colors.white, size: 32.0)
+              ),
+              Positioned(
+                right: 90.0,
+                bottom: 20.0,
+                child: Icon(Icons.ac_unit, color: Colors.white, size: 32.0)
+              )
+            ],
           )
         ],
       ),
@@ -41,9 +72,7 @@ class IconBadge extends StatelessWidget {
   final IconData icon;
   final double size;
 
-  IconBadge(this.icon, {
-    this.size = 32.0
-  });
+  IconBadge(this.icon, {this.size = 32.0});
 
   @override
   Widget build(BuildContext context) {
