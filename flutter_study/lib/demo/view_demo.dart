3f067f9280fc3d8c2606a9be0f4bfa4d4508b8ea
Bruceyuan <664995775@qq.com>
2020-02-25 23:09:51 +0800

    GridView.builder 按需生成视图项目

--- flutter_study/lib/demo/view_demo.dart
+++ flutter_study/lib/demo/view_demo.dart
@@ -2,28 +2,58 @@ import 'package:flutter/material.dart';
 import 'package:flutter_study/model/post.dart';
 
 class ViewDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GridViewBuilderDemo();
+  }
+}
+
+class GridViewBuilderDemo extends StatelessWidget {
+  Widget _gridItemBuilder(BuildContext context, int index) {
+    return Container(
+      child: Image.network(
+        posts[index].imageUrl,
+        fit: BoxFit.cover,
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+      itemCount: posts.length,
+      itemBuilder: _gridItemBuilder,
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 3,
+        crossAxisSpacing: 8.0,
+        mainAxisSpacing: 8.0
+      ),
+    );
+  }
+}
+
+class GridViewCountDemo extends StatelessWidget {
   List<Widget> _buildTiles(int length) {
     return List.generate(length, (int index) {
       return Container(
-          color: Colors.grey[300],
-          alignment: Alignment(0.0, 0.0),
-          child: Text(
-            'Item: $index',
-            style: TextStyle(fontSize: 18.0, color: Colors.grey),
-          ),
-        );
+        color: Colors.grey[300],
+        alignment: Alignment(0.0, 0.0),
+        child: Text(
+          'Item: $index',
+          style: TextStyle(fontSize: 18.0, color: Colors.grey),
+        ),
+      );
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return GridView.count(
-      crossAxisCount: 3,
-      crossAxisSpacing: 16.0,
-      mainAxisSpacing: 16.0,
-      scrollDirection: Axis.vertical,
-      children: _buildTiles(100)
-    );
+        crossAxisCount: 3,
+        crossAxisSpacing: 16.0,
+        mainAxisSpacing: 16.0,
+        scrollDirection: Axis.vertical,
+        children: _buildTiles(100));
   }
 }
 
@@ -41,20 +71,19 @@ class PageViewBuildDemo extends StatelessWidget {
           bottom: 8.0,
           left: 8.0,
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Text(
-                posts[index].title,
-                style: TextStyle(fontWeight: FontWeight.bold),
-              ),
-              Text(
-                posts[index].author,
-              )
-            ]
-          ),
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  posts[index].title,
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  posts[index].author,
+                )
+              ]),
         ),
       ],
-    ); 
+    );
   }
 
   @override
