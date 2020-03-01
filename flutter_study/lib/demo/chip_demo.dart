aa5d9a4fd1b5aff55f6729f417dcc8b67996d53c
Bruceyuan <664995775@qq.com>
2020-03-01 18:03:11 +0800

    FilterChip：过滤碎片

--- flutter_study/lib/demo/chip_demo.dart
+++ flutter_study/lib/demo/chip_demo.dart
@@ -13,6 +13,7 @@ class _ChipDemoState extends State<ChipDemo> {
   ];
 
   String _action = 'Nothing';
+  List<String> _selected = [];
 
   @override
   Widget build(BuildContext context) {
@@ -105,6 +106,33 @@ class _ChipDemoState extends State<ChipDemo> {
                     );
                   }).toList(),
                 ),
+                Divider(
+                  color: Colors.grey,
+                  height: 32.0,
+                  indent: 10,
+                ),
+                Container(
+                  width: double.infinity,
+                  child: Text('FilterChip: ${_selected.toString()}'),
+                ),
+                Wrap(
+                  spacing: 8.0,
+                  children: _tags.map((tag) {
+                    return FilterChip(
+                      label: Text(tag),
+                      selected: _selected.contains(tag),
+                      onSelected: (value) {
+                        setState(() {
+                          if (_selected.contains(tag)) {
+                            _selected.remove(tag);
+                          } else {
+                            _selected.add(tag);
+                          }
+                        });
+                      },
+                    );
+                  }).toList(),
+                ),
               ],
             )
           ],
@@ -119,6 +147,8 @@ class _ChipDemoState extends State<ChipDemo> {
               'Banana',
               'Lemon',
             ];
+
+            _selected = [];
           });
         },
       )
