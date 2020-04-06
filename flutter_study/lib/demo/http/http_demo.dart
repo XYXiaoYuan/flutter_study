fdde14e83f07c956ce5c2bc2c392c71d5c712ccc
Bruceyuan <664995775@qq.com>
2020-04-06 11:29:17 +0800

    JSON 转换（dartconvert）

--- flutter_study/lib/demo/http/http_demo.dart
+++ flutter_study/lib/demo/http/http_demo.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:async';
 
 class HttpDemo extends StatelessWidget {
   @override
@@ -8,9 +12,49 @@ class HttpDemo extends StatelessWidget {
         title: Text('HttpDemo'),
         elevation: 0.0
       ),
-      body: Center(
-        child: Text('HttpDemo'),
-      ),
+      body: HttpDemoHome(),
+    );
+  }
+}
+
+class HttpDemoHome extends StatefulWidget {
+  @override
+  _HttpDemoHomeState createState() => _HttpDemoHomeState();
+}
+
+class _HttpDemoHomeState extends State<HttpDemoHome> {
+  @override
+  void initState() {
+    super.initState();
+    // fetchPost();
+
+    final post = {
+      'title': 'hello',
+      'description': 'nice to meet you'
+    };
+
+    print(post['title']);
+    print(post['description']);
+
+    final postJson = json.encode(post);
+    print(postJson);
+
+    final postJsonConverted = json.decode(postJson);
+    print(postJsonConverted['title']);
+    print(postJsonConverted['description']);
+  }
+
+  void fetchPost() async {
+    final response = 
+        await http.get('https://resources.ninghao.net/demo/posts.json');
+    print('statusCode: ${response.statusCode}');
+    print('body: ${response.body}');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
     );
   }
 }
