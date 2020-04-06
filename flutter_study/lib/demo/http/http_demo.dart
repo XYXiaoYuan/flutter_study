bc737dffea0dd630167e9784f1e0eae5b821ccc0
Bruceyuan <664995775@qq.com>
2020-04-06 11:53:20 +0800

    FutureBuilder：使用 Future 构建小部件

--- flutter_study/lib/demo/http/http_demo.dart
+++ flutter_study/lib/demo/http/http_demo.dart
@@ -8,10 +8,7 @@ class HttpDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('HttpDemo'),
-        elevation: 0.0
-      ),
+      appBar: AppBar(title: Text('HttpDemo'), elevation: 0.0),
       body: HttpDemoHome(),
     );
   }
@@ -26,61 +23,98 @@ class _HttpDemoHomeState extends State<HttpDemoHome> {
   @override
   void initState() {
     super.initState();
-    // fetchPost();
+    // fetchPosts()
+    //   .then((value) => print(value));
 
-    final post = {
-      'title': 'hello',
-      'description': 'nice to meet you'
-    };
+    // final post = {
+    //   'title': 'hello',
+    //   'description': 'nice to meet you'
+    // };
 
-    print(post['title']);
-    print(post['description']);
+    // print(post['title']);
+    // print(post['description']);
 
-    final postJson = json.encode(post);
-    print(postJson);
+    // final postJson = json.encode(post);
+    // print(postJson);
 
-    final postJsonConverted = json.decode(postJson);
-    print(postJsonConverted['title']);
-    print(postJsonConverted['description']);
-    print(postJsonConverted is Map);
+    // final postJsonConverted = json.decode(postJson);
+    // print(postJsonConverted['title']);
+    // print(postJsonConverted['description']);
+    // print(postJsonConverted is Map);
 
-    final postModel = Post.fromJson(postJsonConverted);
-    print('title: ${postModel.title}');
-    print('description: ${postModel.description}');
+    // final postModel = Post.fromJson(postJsonConverted);
+    // print('title: ${postModel.title}');
+    // print('description: ${postModel.description}');
 
-    print('${json.encode(postModel)}');
+    // print('${json.encode(postModel)}');
   }
 
-  void fetchPost() async {
-    final response = 
+  Future<List<Post>> fetchPosts() async {
+    final response =
         await http.get('https://resources.ninghao.net/demo/posts.json');
-    print('statusCode: ${response.statusCode}');
-    print('body: ${response.body}');
+    // print('statusCode: ${response.statusCode}');
+    // print('body: ${response.body}');
+    if (response.statusCode == 200) {
+      final responseBody = json.decode(response.body);
+      List<Post> posts = responseBody['posts']
+          .map<Post>((item) => Post.fromJson(item))
+          .toList();
+      return posts;
+    } else {
+      throw Exception('Failed to fetch posts');
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
-    );
+    return FutureBuilder(
+      future: fetchPosts(),
+      builder: (BuildContext context, AsyncSnapshot snapshot) {
+        print('data: ${snapshot.data}');
+        print('connectionState: ${snapshot.connectionState}');
+
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(
+            child: Text('loading...'),
+          );
+        }
+        
+        return ListView(
+          children: snapshot.data.map<Widget>((item) {
+            return ListTile(
+              title: Text(item.title),
+              subtitle: Text(item.author),
+              leading: CircleAvatar(
+                backgroundImage: NetworkImage(item.imageUrl)
+              )
+            );
+          }).toList(),
+        );
+      });
   }
 }
 
 class Post {
+  final int id;
   final String title;
   final String description;
+  final String author;
+  final String imageUrl;
 
-  Post(
-    this.title,
-    this.description,
-  );
+  Post(this.id, this.title, this.description, this.author, this.imageUrl);
 
   Post.fromJson(Map json)
-  : title = json['title'],
-    description = json['description'];
+      : id = json['id'],
+        title = json['title'],
+        description = json['description'],
+        author = json['author'],
+        imageUrl = json['imageUrl'];
 
   Map toJson() => {
-   'title' : title,
-   'description': description,
-  };
+        'id': id,
+        'title': title,
+        'description': description,
+        'author': author,
+        'imageUrl': imageUrl,
+      };
 }
